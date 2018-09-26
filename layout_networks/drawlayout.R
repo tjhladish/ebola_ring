a4cb273aad0be3ff3e51c216d60563492a97dd24
Carl A. B. Pearson <blank101@gmail.com>
2018-09-26 10:58:58 -0400

    updated network check workflow

--- layout_networks/drawlayout.R
+++ layout_networks/drawlayout.R
@@ -1,23 +1,74 @@
+require(data.table)
+require(ggplot2)
 
-layouts = Sys.glob('./*gauss.layout')
-networks = Sys.glob('../posterior_networks/*gauss.csv')
+args <- c("~/Downloads/sample_networks","adsfasdfasdf4x4.png")
+args <- commandArgs(trailingOnly = TRUE)
 
-levels = read.table('gauss_levels.out')
-colors = c('firebrick','dodgerblue','grey65')
-par(mfrow=c(3,5))
-par(mar=c(0,0,0,0))
-for (i in 1:15) {
-#for (i in 1:length(networks)) {
-#i = 14
-    d = read.table(layouts[i])
-    n = read.table(networks[i])
-    net_id = regmatches(networks[i], regexpr('\\d+', networks[i]))    
-    names(d) = c('id1', 'x1', 'y1')
-    d = d[order(d$id1),]
-    plot(d$x1, d$y1, pch=20, cex=3, col=colors[levels$V3[levels$V1==net_id]+1])
-    names(n) = c('id1', 'id2')
-    layout = merge(n, d)
-    names(d) = c('id2', 'x2', 'y2')
-    layout = merge(layout, d)
-    segments(layout$x1, layout$y1, layout$x2, layout$y2, lwd=2, col='#00000044')
+set.seed(0L)
+
+pth <- args[1]
+tar <- args[2]
+sides <- as.integer(gsub("^.+(\\d+)\\..+$", "\\1", tar))
+
+layouts = list.files(paste0(pth,"/layouts"), pattern = "\\.layout$", full.names = T)
+networks = list.files(paste0(pth,"/posterior_networks"), pattern = "\\.csv$", full.names = T)
+labels = fread(list.files(pth, pattern = "\\.out", full.names = T, include.dirs = F)[1], col.names = c("id","vertex","L"))
+
+net.dt <- rbindlist(lapply(networks, function(fn) {
+  id <- as.integer(gsub("^.+/([^/]+)_.+\\.csv$","\\1",fn))
+  res <- fread(fn, col.names = c("from","to"))[, id := id ]
+  res
+}))
+
+layout.dt <- rbindlist(lapply(layouts, function(fn) {
+  id <- as.integer(gsub("^.+/([^/]+)_.+\\.layout$","\\1",fn))
+  res <- fread(fn, col.names = c("vertex","x","y"))[, id := id ]
+  res
+}))[labels, on=c("id", "vertex")]
+
+positions.dt <- {
+  tmp <- net.dt[layout.dt,
+    .(id, from, to, from.x=x, from.y=y), on=.(from=vertex, id=id)
+  ][!is.na(to)]
+  tmp[layout.dt,
+    .(id, from, to, from.x, from.y, to.x=x, to.y=y), on=.(to=vertex, id=id)
+  ][!is.na(from), .SD, keyby=.(id, from, to)]
 }
+
+picks <- sample(unique(net.dt$id), sides^2)
+
+colors = c(`0`='firebrick',`1`='dodgerblue',`2`='grey65')
+vertices = layout.dt[id %in% picks]
+edges = positions.dt[id %in% picks]
+    
+# ggplot(vertices) +
+#   facet_wrap(~id, scales = "free") +
+#   geom_segment(
+#     aes(x=from.x, y=from.y, xend=to.x, yend=to.y), data=edges,
+#     color="lightgrey", alpha=0.5
+#   ) +
+#   aes(x=x, y=y, color=factor(L)) +
+#   geom_point() +
+#   theme_minimal() + theme(
+#     axis.text = element_blank(), axis.title = element_blank(),
+#     panel.grid = element_blank()
+#   ) + scale_color_manual(guide="none", values = colors)
+
+require(geomnet)
+
+ggsave(tar,
+  ggplot(
+    edges[vertices, .(from, to, id, L), on=.(id, from=vertex)],
+    aes(from_id=as.character(from), to_id=as.character(to), color=as.factor(L))
+  ) +
+    facet_wrap(~id, scales = "free") +
+    geom_net(
+      layout.alg="fruchtermanreingold",
+      directed = FALSE, labelon = FALSE, size = 1,
+      ecolour = "lightgray", ealpha = 0.5,
+      linewidth = 0.2, fiteach = TRUE
+    ) + theme_net() +
+    theme(strip.background = element_blank()) +
+    scale_color_manual(guide="none", values = colors),
+  width = 6, height = 6, units = "in"
+)
